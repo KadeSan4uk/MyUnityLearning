b575dd27a447b0169f09163a2367d3cf384bd7a0
KadeSan4uk <kotejchik@gmail.com>
2024-06-22 00:24:54 +0300

    Moovment Scripts in object Lesson 7

--- Assets/BoxMoover.cs
+++ Assets/BoxMoover.cs
@@ -7,14 +7,31 @@ public class BoxMoover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0,2,0);// передвинуть по позиции
-        transform.eulerAngles = new Vector3(0,45,30);// вращать обьект в пространстве (в градцсах)
-        transform.localScale = new Vector3(2,2,3);// масштабировать обьект 
+        //transform.position = new Vector3(0,2,0);// передвинуть по позиции
+        //transform.eulerAngles = new Vector3(0,45,30);// вращать обьект в пространстве (в градцсах)
+        //transform.localScale = new Vector3(2,2,3);// масштабировать обьект 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 0.03f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -0.03f);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.localScale *= 1.2f;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.localScale *= 0.8f;
+        }
+        
+        transform.localEulerAngles = new Vector3(0, Input.mousePosition.x, 0);
     }
 }
