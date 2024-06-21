4a781f0a63e38b1470b101fcf9851cbf1d76594f
KadeSan4uk <kotejchik@gmail.com>
2024-06-21 23:59:03 +0300

    Transform Methods lesson 6

--- Assets/BoxMoover.cs
+++ Assets/BoxMoover.cs
@@ -7,12 +7,14 @@ public class BoxMoover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(123); 
+        transform.position = new Vector3(0,2,0);// передвинуть по позиции
+        transform.eulerAngles = new Vector3(0,45,30);// вращать обьект в пространстве (в градцсах)
+        transform.localScale = new Vector3(2,2,3);// масштабировать обьект 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello");
+
     }
 }
