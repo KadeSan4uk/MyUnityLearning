1b5bb8b481ce3f12500b19f64f6a78ee6eabdeba
KadeSan4uk <kotejchik@gmail.com>
2024-06-21 23:42:46 +0300

    FirstScript Lesson5

---
+++ Assets/BoxMoover.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoxMoover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(123); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello");
+    }
+}
