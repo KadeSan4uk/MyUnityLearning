cc20cd5ad47a97432cceb0bb4b0c789c16864b6e
KadeSan4uk <kotejchik@gmail.com>
2024-06-23 01:19:41 +0300

    runner1 Lesson 8

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed = 100f;
+    private float minRotation = -70f;
+    private float maxRotation = 70f;
+    [SerializeField] private Camera _camera;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    // Time.deltaTime оптимизирует движение обьекта не зависимо от частоты кадров
+    {
+
+        _camera.transform.position = transform.position + new Vector3(0.001f, 3.574f, -10);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 newPosition = transform.position + transform.forward * Time.deltaTime * _speed;
+            newPosition.x = Mathf.Clamp(newPosition.x, -2.9f, 2.9f);
+            transform.position = newPosition;
+
+        }
+
+        float currentYRotation = transform.localEulerAngles.y;
+
+        // Преобразуем угол в диапазон от -180 до 180 градусов
+        if (currentYRotation > 180)
+        {
+            currentYRotation -= 360;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentYRotation -= _rotationSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            currentYRotation += _rotationSpeed * Time.deltaTime;
+        }
+
+        // Ограничиваем угол поворота
+        currentYRotation = Mathf.Clamp(currentYRotation, minRotation, maxRotation);
+
+        // Устанавливаем новый угол поворота
+        transform.localRotation = Quaternion.Euler(0, currentYRotation, 0);
+    }
+}
