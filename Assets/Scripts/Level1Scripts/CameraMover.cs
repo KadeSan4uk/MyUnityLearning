ebfba0672c4c3b5dfa47a7613677408ed054400b
KadeSan4uk <kotejchik@gmail.com>
2024-07-01 22:50:43 +0300

    Adding prefab Scene

--- Assets/Scripts/Level1Scripts/CameraMover.cs
+++ Assets/Scripts/Level1Scripts/CameraMover.cs
@@ -5,6 +5,12 @@ public class CameraMover : MonoBehaviour
 {
     [SerializeField] Transform _target;
 
+    private void Start()
+    {
+        //отвязка камеры от Плеера как дочерняя при старте
+        transform.parent = null;
+    }
+
     void LateUpdate()
     {        
         if (_target)
--- Assets/Scripts/Level1Scripts/GameManager.cs
+++ Assets/Scripts/Level1Scripts/GameManager.cs
@@ -1,10 +1,18 @@
+using TMPro;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject _startMenu;
+    [SerializeField] TextMeshProUGUI _levelText;
+
+    private void Start()
+    {
+        _levelText.text = SceneManager.GetActiveScene().name;
+    }
     public void PLay()
     {
         _startMenu.SetActive(false);
