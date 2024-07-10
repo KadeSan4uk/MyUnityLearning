73c521508f6fe4ef649921f609bcfea89d006dba
KadeSan4uk <kotejchik@gmail.com>
2024-07-10 21:09:07 +0300

    Revert "Adding sounds effects"
    This reverts commit a1d60938f83fc1f8f49b3f3cfab9ada1e6458f90.

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 public class Barrier : MonoBehaviour
 {
     [SerializeField] private GameObject _bricksEffectPrefab;
+    [SerializeField] private AudioSource _breakWall;
     private void OnTriggerEnter(Collider other)
     {
         PlayerModifire playerModifaer = other.attachedRigidbody.GetComponent<PlayerModifire>();
 
         if (playerModifaer)
         {
+            _breakWall.Play();
             playerModifaer.HitBarrierHeigth();
             playerModifaer.HitBarrierWitdth();
             Destroy(gameObject);
